13e896245fbf88bbb8a3db0565d628b90fcc8dd4
onreK <kernopay@gmail.com>
2025-06-23 19:28:40 -0400

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,29 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  // Routes that can be accessed while signed out
+  publicRoutes: [
+    "/",
+    "/sign-in(.*)",
+    "/sign-up(.*)",
+    "/api/webhooks(.*)"
+  ],
+  // Routes that can always be reached, and have 
+  // no authentication information
+  ignoredRoutes: [
+    "/((?!api|trpc))(_next.*|.+.[w]+$)",
+    "/api/webhooks(.*)"
+  ]
+});
+
+export const config = {
+  // Protects all routes, including api/trpc.
+  // See https://clerk.com/docs/references/nextjs/auth-middleware
+  // for more information about configuring your Middleware
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!.+\\.[\\w]+$|_next).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)'
+  ]
+};
